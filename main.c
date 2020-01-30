f8d3c23e1caac451284088dafc9375e24207fee1
Nazar <lozynskyi.nazar@gmail.com>
2020-01-30 09:50:24 +0200

    first task

---
+++ main.c
@@ -0,0 +1,229 @@
+#include <stdlib.h>
+#include "main.h"
+#include <pthread.h>
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+pthread_mutex_t mutexInputsGroup1;
+pthread_mutex_t mutexInputsGroup2;
+pthread_mutex_t mutexTempSensors;
+
+void *randomizeInputPorts(void *args)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutexInputsGroup1);
+        uint16_t randomNumber = rand()%1023;
+        ioState.inputPort1 = randomNumber&(1<<0);
+        ioState.inputPort2 = randomNumber&(1<<1);
+        ioState.inputPort3 = randomNumber&(1<<2);
+        ioState.inputPort4 = randomNumber&(1<<3);
+        ioState.inputPort5 = randomNumber&(1<<4);
+        ioState.inputPort6 = randomNumber&(1<<5);
+        ioState.inputPort7 = randomNumber&(1<<6);
+        ioState.inputPort8 = randomNumber&(1<<7);
+        pthread_mutex_unlock(&mutexInputsGroup1);
+        
+        pthread_mutex_lock(&mutexInputsGroup2);
+        ioState.inputPort9 = randomNumber&(1<<8);
+        ioState.inputPort10 = randomNumber&(1<<9);
+        pthread_mutex_unlock(&mutexInputsGroup2);
+        usleep(1000000);
+    }
+    return NULL;
+}
+
+void *changeTempSensorsVal(void *args)
+{
+    while(1)
+    {
+        uint8_t i;
+        for(i = 0; i < 255; ++i)
+        {
+            pthread_mutex_lock(&mutexTempSensors);
+            ioState.tempSensor1 = i;
+            ioState.tempSensor2 = abs(i - 255);
+            pthread_mutex_unlock(&mutexTempSensors);
+            usleep(50000);
+        }
+        for(i = 255; i > 0; --i)
+        {
+            pthread_mutex_lock(&mutexTempSensors);
+            ioState.tempSensor1 = i;
+            ioState.tempSensor2 = abs(i - 255);
+            pthread_mutex_unlock(&mutexTempSensors);
+            usleep(50000);
+        }
+    }
+    return NULL;
+}
+
+void *printState(void *args)
+{
+    while(1)
+    {
+        pthread_mutex_lock(&mutexInputsGroup1);
+        printf("%d",ioState.inputPort1);
+        printf("\t%u\n",ioState.outputPort1);
+        printf("%d",ioState.inputPort2);
+        printf("\t%u\n",ioState.outputPort2);
+        printf("\n");
+        printf("%d",ioState.inputPort3);
+        printf("\t%u\n",ioState.outputPort3);
+        printf("%d",ioState.inputPort4);
+        printf("\t%u\n",ioState.outputPort4);
+        printf("%d",ioState.inputPort5);
+        printf("\t%u\n",ioState.outputPort5);
+        printf("%d",ioState.inputPort6);
+        printf("\t%u\n",ioState.outputPort6);
+        printf("%d",ioState.inputPort7);
+        printf("\t%u\n",ioState.outputPort7);
+        printf("%d",ioState.inputPort8);
+        printf("\t%u\n",ioState.outputPort8);
+        printf("\n");
+        pthread_mutex_unlock(&mutexInputsGroup1);
+
+        pthread_mutex_lock(&mutexInputsGroup2);
+        pthread_mutex_lock(&mutexTempSensors);
+        printf("%d",ioState.inputPort9);
+        printf("  %d",ioState.tempSensor1);
+        printf("\t%u\n",ioState.outputPort9);
+
+        printf("%d",ioState.inputPort10);
+        printf("  %d",ioState.tempSensor2);
+        printf("\t%u\n",ioState.outputPort10);
+        pthread_mutex_unlock(&mutexInputsGroup2);
+        pthread_mutex_unlock(&mutexTempSensors);
+        printf("\n\n\n\n");
+        usleep(500000);
+    }
+    return NULL;
+}
+
+void *toggleOutput(void *port)
+{
+    bool *localPort = (bool*) port;
+    *localPort = !*localPort;
+    return NULL;
+}
+
+void *setOutput(void *port)
+{
+    bool *localPort = (bool*) port;
+    *localPort = 1;
+    return NULL;
+}
+
+void *resetOutput(void *port)
+{
+    bool *localPort = (bool*) port;
+    *localPort = 0;
+    return NULL;
+}
+
+void *controller(void *args)
+{
+    uint8_t tempRule1 = 0;
+    uint8_t tempRule2 = 0;
+    uint8_t tempRule3 = 0;
+    uint8_t tempRule4 = 0;
+    while(1)
+    {
+        //Rule 1
+        pthread_mutex_lock(&mutexInputsGroup1);
+        if(ioState.inputPort1 & ioState.inputPort2 & !tempRule1)
+        {
+            pthread_t thread_id1, thread_id2;
+            pthread_create(&thread_id1, NULL, toggleOutput, &(ioState.outputPort1));
+            pthread_create(&thread_id2, NULL, toggleOutput, &(ioState.outputPort2));
+            tempRule1 = 1;
+        }
+        if(!ioState.inputPort1 & !ioState.inputPort2 & tempRule1)
+        {
+            pthread_t thread_id1, thread_id2;
+            pthread_create(&thread_id1, NULL, toggleOutput, &(ioState.outputPort1));
+            pthread_create(&thread_id2, NULL, toggleOutput, &(ioState.outputPort2));
+            tempRule1 = 0;
+        }
+
+        //Rule 2
+        if((ioState.inputPort3 + ioState.inputPort4 + ioState.inputPort5
+            + ioState.inputPort6 + ioState.inputPort7 + ioState.inputPort8) > 3
+           & !tempRule2)
+        {
+            pthread_t thread_id[6];
+            pthread_create(&thread_id[0], NULL, setOutput, &(ioState.outputPort3));
+            pthread_create(&thread_id[1], NULL, setOutput, &(ioState.outputPort4));
+            pthread_create(&thread_id[2], NULL, setOutput, &(ioState.outputPort5));
+            pthread_create(&thread_id[3], NULL, setOutput, &(ioState.outputPort6));
+            pthread_create(&thread_id[4], NULL, setOutput, &(ioState.outputPort7));
+            pthread_create(&thread_id[5], NULL, setOutput, &(ioState.outputPort8));
+            tempRule2 = 1;
+        }
+        if((ioState.inputPort3 + ioState.inputPort4 + ioState.inputPort5
+            + ioState.inputPort6 + ioState.inputPort7 + ioState.inputPort8) <= 3
+           & tempRule2)
+        {
+            pthread_t thread_id[6];
+            pthread_create(&thread_id[0], NULL, setOutput, &(ioState.outputPort3));
+            pthread_create(&thread_id[1], NULL, resetOutput, &(ioState.outputPort4));
+            pthread_create(&thread_id[2], NULL, setOutput, &(ioState.outputPort5));
+            pthread_create(&thread_id[3], NULL, resetOutput, &(ioState.outputPort6));
+            pthread_create(&thread_id[4], NULL, setOutput, &(ioState.outputPort7));
+            pthread_create(&thread_id[5], NULL, resetOutput, &(ioState.outputPort8));
+            tempRule2 = 0;
+        }
+        pthread_mutex_unlock(&mutexInputsGroup1);
+
+
+        //Rule 3
+        pthread_mutex_lock(&mutexTempSensors);
+        pthread_mutex_lock(&mutexInputsGroup2);
+        if(ioState.inputPort9 & ioState.tempSensor1 > 127 & !tempRule3)
+        {
+            pthread_t thread_id1;
+            pthread_create(&thread_id1, NULL, setOutput, &(ioState.outputPort9));
+            tempRule3 = 1;
+        }
+        if(ioState.tempSensor1 <= 127 & tempRule3)
+        {
+            pthread_t thread_id1;
+            pthread_create(&thread_id1, NULL, resetOutput, &(ioState.outputPort9));
+            tempRule3 = 0;
+        }
+
+        //Rule 4
+        if(ioState.inputPort9 & ioState.inputPort10 & ioState.tempSensor2 > 127 & !tempRule4)
+        {
+            pthread_t thread_id1;
+            pthread_create(&thread_id1, NULL, setOutput, &(ioState.outputPort10));
+            tempRule4 = 1;
+        }
+        if(ioState.tempSensor2 <= 127 & tempRule4)
+        {
+            pthread_t thread_id1;
+            pthread_create(&thread_id1, NULL, resetOutput, &(ioState.outputPort10));
+            tempRule4 = 0;
+        }
+        pthread_mutex_unlock(&mutexTempSensors);
+        pthread_mutex_unlock(&mutexInputsGroup2);
+        usleep(1);
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_mutex_init(&mutexInputsGroup1, NULL);
+    pthread_mutex_init(&mutexInputsGroup2, NULL);
+    pthread_mutex_init(&mutexTempSensors, NULL);
+
+    pthread_t thread_id[4];
+    pthread_create(&thread_id[0], NULL, randomizeInputPorts, NULL);
+    pthread_create(&thread_id[1], NULL, changeTempSensorsVal, NULL);
+    pthread_create(&thread_id[2], NULL, printState, NULL);
+    pthread_create(&thread_id[3], NULL, controller, NULL);
+    pthread_join(thread_id[3], NULL);
+    exit(0);
+}
---
+++ main.h
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+struct
+{
+    bool inputPort1;
+    bool inputPort2;
+    bool inputPort3;
+    bool inputPort4;
+    bool inputPort5;
+    bool inputPort6;
+    bool inputPort7;
+    bool inputPort8;
+    bool inputPort9;
+    bool inputPort10;
+    bool outputPort1;
+    bool outputPort2;
+    bool outputPort3;
+    bool outputPort4;
+    bool outputPort5;
+    bool outputPort6;
+    bool outputPort7;
+    bool outputPort8;
+    bool outputPort9;
+    bool outputPort10;
+    uint8_t tempSensor1;
+    uint8_t tempSensor2;
+} ioState;
+
+
